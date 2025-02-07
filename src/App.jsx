5cae33628780c6df7c39cb51918bab109060a4b5
IvanTorzoni <torzoni.ivan00@gmail.com>
2025-02-07 16:54:54 +0100

    feat: added result component and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
+import { useState } from "react"
+import Result from "./components/Result"
 
 function App() {
+  //Crea uno state con un oggetto JS
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  //crea la funzione al cambio del valore dove inputIdentifier è l' oggetto dello state e new value sarà il valore inserito dall'utente
+  function handleChange(inputIdentifier, newValue) {
+    //setto userInput dicendo che il valore precedente lo deve prendere dalla copia dell' oggetto JS dello state (creo un nuovo oggetto tramite spread operator) e l'identifier sarà il nuovo valore dell' utente
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue
+      }
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      {/* al componente UserInput assegno delle props, userInput sara il valore che avra quell'input, onChange richiama la funzione che permetterà di cambiare il valore all' input */}
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {/* al componente Result assegno delle props, input sara il valore iniziale dell' oggetto o il valore inserito dall' utente */}
+      <Result input={userInput} />
     </>
   )
 
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,46 +1,25 @@
-import { useState } from "react"
-
-//crea la funzione per l'oggetto. 
-export default function UserInput() {
-    //Crea uno state con un oggetto JS
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10,
-    })
-
-    //crea la funzione al cambio del valore dove inputIdentifier è l' oggetto dello state e new value sarà il valore inserito dall'utente
-    function handleChange(inputIdentifier, newValue) {
-        //setto userInput dicendo che il valore precedente lo deve prendere dalla copia dell' oggetto JS dello state (creo un nuovo oggetto tramite spread operator) e l'identifier sarà il nuovo valore dell' utente
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier]: newValue
-            }
-        });
-    }
-
+//crea la funzione per l'oggetto. Faccio accettare alla funzione delle prop che arrivano da app.jsx
+export default function UserInput({onChange, userInput}) {
     return <section id="user-input">
         <div className="input-group">
-            {/* creo gli input dove value sarà inizialmente il valore dell'oggetto mentro al richiamo della funzione tramite onChange, il valore verrà modificato con il nuovo valore inserito dall' utente*/}
+            {/* creo gli input dove VALUE sarà inizialmente il valore dell'oggetto che arriva da app.jsx o il valore modificato, mentre ONCHANGE prenderà il valore che inserisce l'utente da app.jsx */}
             <p>
                 <label>Initial Investment</label>
-                <input type="number" required value={userInput.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)}/>
+                <input type="number" required value={userInput.initialInvestment} onChange={(event) => onChange('initialInvestment', event.target.value)}/>
             </p>
             <p>
                 <label>Annual Investment</label>
-                <input type="number" required value={userInput.annualInvestment} onChange={(event) => handleChange('annualInvestment', event.target.value)}/>
+                <input type="number" required value={userInput.annualInvestment} onChange={(event) => onChange('annualInvestment', event.target.value)}/>
             </p>
         </div>
         <div className="input-group">
             <p>
                 <label>Expected Return</label>
-                <input type="number" required value={userInput.expectedReturn} onChange={(event) => handleChange('expectedReturn', event.target.value)}/>
+                <input type="number" required value={userInput.expectedReturn} onChange={(event) => onChange('expectedReturn', event.target.value)}/>
             </p>
             <p>
                 <label>Duration</label>
-                <input type="number" required value={userInput.duration} onChange={(event) => handleChange('duration', event.target.value)}/>
+                <input type="number" required value={userInput.duration} onChange={(event) => onChange('duration', event.target.value)}/>
             </p>
         </div>
     </section>
