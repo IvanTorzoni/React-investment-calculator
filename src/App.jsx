2012fc764c6ee4fe13e09e25612483300dde7fee
IvanTorzoni <torzoni.ivan00@gmail.com>
2025-02-07 17:22:48 +0100

    feat: dynamic table message

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ function App() {
     duration: 10,
   })
 
+  //creo una costante per mostrare la tabella in modo dinamico
+  const inputIsValid = userInput.duration >= 1
+
   //crea la funzione al cambio del valore dove inputIdentifier è l' oggetto dello state e new value sarà il valore inserito dall'utente
   function handleChange(inputIdentifier, newValue) {
     //setto userInput dicendo che il valore precedente lo deve prendere dalla copia dell' oggetto JS dello state (creo un nuovo oggetto tramite spread operator) e l'identifier sarà il nuovo valore dell' utente
@@ -30,7 +33,9 @@ function App() {
       {/* al componente UserInput assegno delle props, userInput sara il valore che avra quell'input, onChange richiama la funzione che permetterà di cambiare il valore all' input */}
       <UserInput userInput={userInput} onChange={handleChange} />
       {/* al componente Result assegno delle props, input sara il valore iniziale dell' oggetto o il valore inserito dall' utente */}
-      <Result input={userInput} />
+      {/* tramite la costante posso mostrare la tabella o il messaggio in base al valore inserito */}
+      {inputIsValid && <Result input={userInput} />}
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero</p>}
     </>
   )
 
