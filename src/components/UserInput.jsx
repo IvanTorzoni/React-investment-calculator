726bb4b4a958f3721cd3497a07c7db3d24e32478
IvanTorzoni <torzoni.ivan00@gmail.com>
2025-02-07 16:38:02 +0100

    feat: user input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 
+//crea la funzione per l'oggetto. 
 export default function UserInput() {
+    //Crea uno state con un oggetto JS
     const [userInput, setUserInput] = useState({
         initialInvestment: 10000,
         annualInvestment: 1200,
@@ -8,7 +10,9 @@ export default function UserInput() {
         duration: 10,
     })
 
+    //crea la funzione al cambio del valore dove inputIdentifier è l' oggetto dello state e new value sarà il valore inserito dall'utente
     function handleChange(inputIdentifier, newValue) {
+        //setto userInput dicendo che il valore precedente lo deve prendere dalla copia dell' oggetto JS dello state (creo un nuovo oggetto tramite spread operator) e l'identifier sarà il nuovo valore dell' utente
         setUserInput(prevUserInput => {
             return {
                 ...prevUserInput,
@@ -19,6 +23,7 @@ export default function UserInput() {
 
     return <section id="user-input">
         <div className="input-group">
+            {/* creo gli input dove value sarà inizialmente il valore dell'oggetto mentro al richiamo della funzione tramite onChange, il valore verrà modificato con il nuovo valore inserito dall' utente*/}
             <p>
                 <label>Initial Investment</label>
                 <input type="number" required value={userInput.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)}/>
